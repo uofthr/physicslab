76a47b3e0947a7c300f78d303590f6c8b22e1d85
Hanno Rein <hanno@hanno-rein.de>
2016-11-01 10:49:24 -0400

    test

--- createuser.bash
+++ createuser.bash
@@ -5,6 +5,8 @@ sudo dscl . -create /Users/research RealName "Research"
 sudo dscl . -create /Users/research UniqueID "510"
 sudo dscl . -create /Users/research PrimaryGroupID 20
 sudo dscl . -create /Users/research NFSHomeDirectory /Users/research
+
+echo "Enter new research password"
 sudo dscl . -passwd /Users/research 
 
 sudo systemsetup -setremotelogin on
@@ -20,9 +22,14 @@ sudo mv gmond.conf /usr/local/etc/gmond.conf
 curl -O https://raw.githubusercontent.com/uofthr/physicslab/master/su.cron
 sudo crontab su.cron
 
+whoami
+echo "su to research"
+
 su research
 
-ssh-keygen
+whoami
+
+cat /dev/zero | ssh-keygen -q -N ""
 
 cd .ssh
 
