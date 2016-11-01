b885db8ee1ba885204c7aa70829b78aa6e0241b4
Hanno Rein <hanno@hanno-rein.de>
2016-11-01 13:08:41 -0400

    test

--- createuser.bash
+++ createuser.bash
@@ -5,7 +5,9 @@ sudo dscl . -create /Users/research RealName "Research"
 sudo dscl . -create /Users/research UniqueID "510"
 sudo dscl . -create /Users/research PrimaryGroupID 20
 sudo dscl . -create /Users/research NFSHomeDirectory /Users/research
-sudo createhomedir -u research
+
+sudo cp -R /System/Library/User\ Template/English.lproj /Users/research
+sudo chown -R research:staff /Users/research
 
 echo "Enter new research password"
 sudo dscl . -passwd /Users/research 
@@ -26,6 +28,7 @@ sudo crontab su.cron
 sudo -u research whoami
 
 sudo -u research mkdir /Users/research/.ssh
+
 sudo -u research ssh-keygen -q -N "" -f /Users/research/.ssh/id_rsa
 
 curl https://raw.githubusercontent.com/uofthr/physicslab/master/authorized_keys --output /Users/research/.ssh/authorized_keys
