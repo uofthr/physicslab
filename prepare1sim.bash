7d7ff2ef0e5da512002d702ba59ebdba95750f9b
Hanno Rein <hanno@hanno-rein.de>
2016-11-02 20:54:01 -0400

    more

--- prepare1sim.bash
+++ prepare1sim.bash
@@ -6,4 +6,4 @@ curl -Lk https://github.com/hannorein/rebound/archive/fast_simulation_restarter.
 unzip rebound.zip
 cd rebound-fast_simulation_restarter/problems/2hour
 curl -Lk https://github.com/uofthr/physicslab/blob/master/restart_0051.bin?raw=true --output restart_0051.bin
-
+make
