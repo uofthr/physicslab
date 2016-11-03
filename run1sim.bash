2171d6533cdc918f83dc647d4f4177761d3a2bff
Hanno Rein <hanno@hanno-rein.de>
2016-11-02 21:07:45 -0400

    run

--- run1sim.bash
+++ run1sim.bash
@@ -3,10 +3,10 @@ cd $HOME
 cd run1sim/rebound-fast_simulation_restarter/problems/
 cd 2hour0
 nohup ./rebound > /dev/null 2>&1 &
-cd 2hour1
+cd ../2hour1
 nohup ./rebound > /dev/null 2>&1 &
-cd 2hour2
+cd ../2hour2
 nohup ./rebound > /dev/null 2>&1 &
-cd 2hour3
+cd ../2hour3
 nohup ./rebound > /dev/null 2>&1 &
 
