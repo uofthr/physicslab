8588637a495bd657a360b822bff1d5f050b9fd17
Hanno Rein <hanno@hanno-rein.de>
2016-11-07 13:22:20 -0500

    1

--- finddown.bash
+++ finddown.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in `seq -f "%02g"  0 44`; do
+for i in `seq -f "%02g"  1 44`; do
     [[ $(ssh -t research@physics-lab${i}.utsc-labs.utoronto.ca "echo 'testecho'" 2>/dev/null)  != *testecho* ]]
     if [ $? -eq 0 ]; then
         echo "research@physics-lab${i}.utsc-labs.utoronto.ca"
