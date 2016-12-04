39c6f3742a26259b7ab7476f68fce1890b80fb8b
Hanno Rein <hanno@hanno-rein.de>
2016-12-03 19:07:25 -0500

    submit

--- submitjob.py
+++ submitjob.py
@@ -3,7 +3,13 @@ conn = sqlite3.connect('jobs.db')
 c = conn.cursor()
 c.execute("DROP TABLE jobs")
 c.execute('''CREATE TABLE jobs (id integer primary key autoincrement, date text, commandpre text, command text, commandpost text, commandpause text, host text, status int)''')
-c.execute('''INSERT INTO jobs VALUES (NULL, '2016-12-03', 'nohup sh -c "sleep 10 && touch /tmp/done.tag || touch /tmp/error.tag" > /dev/null &',  'nohup sh -c "sleep 10 && touch /tmp/done.tag || touch /tmp/error.tag" >/dev/null &', 'nohup sh -c "sleep 10 && touch /tmp/done.tag || touch /tmp/error.tag" >/dev/null &',  'killall rebound', NULL, 0)''')
+
+precommand = "bash <(curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/uofthr/ss_lab1/master/pre.bash?a"
+command = "cd /tmp/rebound/problems/ss_lab1 && rebound && touch /tmp/done.tag || touch /tmp/error.tag"
+postcommand = "uptime"
+commandpause = "killall rebound"
+
+c.execute('INSERT INTO jobs VALUES (NULL, '2016-12-03', "%s", "%s", "%s", "%s", NULL, 0)'%(precommand,command,postcommand,commandpause))
 
 for row in c.execute('SELECT * FROM jobs ORDER BY date'):
     print(row)
